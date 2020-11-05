fd3a61c4d895880ffe249958e4dd5ae79eac4eca
Amil Masic <amil.masic@gmail.com>
2020-11-05 23:51:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,48 +1,27 @@
 class BankAccount
-  attr_accessor :balance, :status
   attr_reader :name
-    def initialize(name)
-      @name = name
-      @balance = 1000
-      @status = "open"
-    end
+  attr_accessor :balance, :status
 
-    def deposit(amount)
-      recent_balance = self.balance += amount
-    end
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
 
-    def display_balance
-      "Your balance is $#{self.balance}."
-    end
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
+  end
 
-    def valid?
-     balance > 0 && status == "open"
-    end
+  def display_balance
+    "Your balance is $#{balance}."
+  end
 
-    def close_account
-      self.status = "closed"
-    end
-
-
-
-    def deposit(amount)
-      recent_balance = @balance += amount
-    end
-
-    def display_balance
-      "Your balance is $#{self.balance}."
-    end
-
-    def valid?
-        !(self.broken && self.close_account)
-
-    end
-
-    def close_account
+  def close_account
     self.status = "closed"
-    end
+  end
 
-    def broken
-      @balance < 1
-    end
+  def valid?
+    balance > 0 && status == "open"
+  end
+
 end
