3a63b68adc09f5559ff463c88858233f44f4069c
Amil Masic <amil.masic@gmail.com>
2020-11-05 23:47:04 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -6,6 +6,7 @@ class BankAccount
       @balance = 1000
       @status = "open"
     end
+# <<<<<<< HEAD
 
     def deposit(amount)
       recent_balance = self.balance += amount
@@ -28,4 +29,26 @@ class BankAccount
     end
 
 
+# >>>>>>> 6ea0516dff7c91cb016af7612d88f729fca6ccc6
+
+    def deposit(amount)
+      recent_balance = @balance += amount
+    end
+
+    def display_balance
+      "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        !(self.broken && self.close_account)
+
+    end
+
+    def close_account
+      @status = "closed"
+    end
+
+    def broken
+      @balance < 1
+    end
 end
